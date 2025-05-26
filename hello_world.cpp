8009275303a2921e384689ea638e00244e69c17f
Guppo207 <kirill.sherstyuk06@mail.ru>
2025-05-26 16:08:15 +0000

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 int main(){
-	std::string name;
-	std::cin>>name;
-	std::cout<<"Hello world from "<<name<<std::endl;
+	std::string name;//имя
+	std::cin>>name;//ввод
+	std::cout<<"Hello world from "<<name<<std::endl;//вывод
 	return 1;
 
 }
